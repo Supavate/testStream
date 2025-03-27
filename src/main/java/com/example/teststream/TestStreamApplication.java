3a12023362c7f34074874c94f54dcca8502f4dce
sahachat <2310511101071@live4.utcc.ac.th>
2025-03-27 11:28:13 +0700

    First commit

---
+++ src/main/java/com/example/teststream/TestStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.teststream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestStreamApplication.class, args);
+    }
+
+}
